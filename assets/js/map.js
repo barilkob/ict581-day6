f588dfb1d3a7f41f0fe126295f9b57ba7e09e448
dennisl67 <embeddedfw@gmail.com>
2016-05-17 19:42:53 -0700

    added google maps api

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,23 @@
 // Initialize google maps
-function initMap() {
+$(document).ready(function(){
+    var marker;
+    var geocoder = new google.maps.Geocoder();
+    var myLatLng = new google.maps.LatLng(51.0486, -114.0708);
+    var mapOptions = {
+        center: myLatLng,
+        disableDoubleClickZoom: true,
+        draggable: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        scrollwheel: false,
+        zoom: 10,
+        zoomControl: false,
+    };
+
+    var map = new google.maps.Map($("#map").get(0), mapOptions);
+});
+
+function initMap2() {
     var mapDiv = document.getElementById("map");
     var map = new google.maps.Map(mapDiv, {
         center: {lat: 51.0486, lng: -114.0708},
